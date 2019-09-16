903559426f7e7748aeea6170e01b33a46a658337
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2019-09-17 01:58:49 +0200

    Added minimal setup.py
    Makes it possible to install using pip:
    
    ```
    $ pip install .
    ```
    
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cocoapy',
+      version='0.0.1',
+      description='Python wrapper around Objective-C runtime',
+      author='Phillip Nguyen',
+      url='https://github.com/phillip-nguyen/cocoa-python',
+      packages=['cocoapy'],
+     )
